dafeb47d924b71595c8d698ec8befb3df4bd893a
Jordan Kwan <jordan.kwan72@gmail.com>
2024-08-19 16:04:48 -0600

    Added images

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -66,7 +66,13 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            image
+            <Image
+              src="https://s3-inventorymanagement-jk.s3.us-west-2.amazonaws.com/profile.jpg"
+              alt="Profile"
+              width={50}
+              height={50}
+              className="rounded-full h-full object-cover"
+            />
             <span className="font-semibold">Jordan Kwan</span>
           </div>
         </div>
--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -79,7 +79,13 @@ const Sidebar = () => {
           isSidebarCollapsed ? "px-5" : "px-8"
         }`}
       >
-        <div>logo</div>
+        <Image
+          src="https://s3-inventorymanagement-jk.s3.us-west-2.amazonaws.com/logo.png"
+          alt="jkstock-logo"
+          width={27}
+          height={27}
+          className="rounded w-8"
+        />
         <h1
           className={`${
             isSidebarCollapsed ? "hidden" : "block"
